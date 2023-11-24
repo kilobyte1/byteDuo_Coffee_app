ccc91e50a7d8fd16230faeb8c6f6117928a11e37
naage <p2562416@my365.dmu.ac.uk>
2023-11-24 14:02:02 +0000

    Changed layout to use fragment on the menu page.

---
+++ app/src/main/java/com/example/byteduo/UI/Basket.kt
@@ -0,0 +1,19 @@
+package com.example.byteduo.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.byteduo.R
+
+class Basket : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basket)
+
+
+
+
+
+
+    }
+}
